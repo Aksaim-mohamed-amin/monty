f1aedfb8f35c3bd87cd084ea57297823f9c8b7a2
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-22 10:18:38 -0700

    Initializastion

---
+++ monty.h
@@ -0,0 +1,15 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/* Libraries */
+#include <stdio.h>
+
+/* Structurs */
+
+/* Static Variables */
+
+/* Main Functions */
+
+/* Help FUnctions */
+
+#endif /* MONTY_H */
